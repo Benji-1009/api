a4a25d8c16981b69e3adf64ed93f046e83b1e29f
Benjamín Paredes Ramírez <benjamin.paredesr@outlook.com>
2025-08-02 01:24:34 -0600

    una prueba que no funcionó

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 // Conexión a MySQL
-const sequelize = new Sequelize('app', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+const sequelize = new Sequelize("app", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
   port: 3306, // Cambia a 3307 si usas XAMPP con MySQL en otro puerto
   logging: false, // Desactiva logs en consola (opcional)
 });
